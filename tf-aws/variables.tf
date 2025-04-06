39766ff01279dcd303a33b0ee38b620948f2543f
shashikumar786 <shashikumar.maroju@gmail.com>
2025-04-07 01:21:01 +0530

    create variables

---
+++ tf-aws/variables.tf
@@ -0,0 +1,10 @@
+# variables.tf
+variables "aws_access_key" {
+    type = string
+    description = "enter aws_access_key"
+}
+
+variables "aws_secret_key" {
+    type = string
+    description = "enter aws_secret_key"
+}
